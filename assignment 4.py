1ba3ba0737ac98001040848b663be8d2e31a9b0f
huzzi3330-cell <huzzi3330@gmail.com>
2026-08-18 02:18:19 +0500

    Add files via upload

---
+++ assignment 4.py
@@ -0,0 +1,24 @@
+Python 3.13.5 (tags/v3.13.5:6cb20a2, Jun 11 2025, 16:15:46) [MSC v.1943 64 bit (AMD64)] on win32
+Enter "help" below or click "Help" above for more information.
+>>> 
+= RESTART: C:/Users/USAMA/AppData/Local/Programs/Python/Python313/assignment 4.py
+
+===== STUDENT RECORDS MANAGEMENT SYSTEM =====
+1. Add Student
+2. Display Students
+3. Search Student
+4. Update Record
+5. Delete Record
+6. Sort Records by Marks
+7. Exit
+Enter your choice: 
+= RESTART: C:/Users/USAMA/AppData/Local/Programs/Python/Python313/assignment 4.py
+
+===== STUDENT RECORDS MANAGEMENT SYSTEM =====
+1. Add Student
+2. Display Students
+3. Search Student
+4. Update Record
+5. Delete Record
+6. Sort Records by Marks
+7. Exit
